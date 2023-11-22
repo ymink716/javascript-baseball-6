5c041b22ff9e46a3f3b4963aee1ebc48cb17f414
ymink716 <ymink716@gmail.com>
2023-11-23 02:05:23 +0900

    test: comparator 테스트 코드 작성

--- __tests__/BaseballNumbersComparator.test.ts
+++ __tests__/BaseballNumbersComparator.test.ts
@@ -1,27 +1,45 @@
 import BaseballNumbersComparator from '../src/models/BaseballNumbersComparator';
+import BaseballNumbers from '../src/models/vo/BaseballNumbers';
+import BaseballNumber from '../src/models/vo/baseballNumber';
 
 describe("BaseballNumbersComparator", () => {
   describe("countStrikes()", () => {
-    test("두 개의 리스트에서 값과 위치가 같은 요소의 개수를 반환한다.", () => {
-      const guessedNumbers = [1, 5, 3];
-      const answerNumbers = [1, 3, 7];
+    test("두 리스트에서 값과 위치가 같은 요소의 개수를 반환한다.", () => {
+      const guess = new BaseballNumbers([
+        new BaseballNumber(1),
+        new BaseballNumber(5),
+        new BaseballNumber(3),
+      ]);
+      const answer = new BaseballNumbers([
+        new BaseballNumber(1),
+        new BaseballNumber(3),
+        new BaseballNumber(7),
+      ]);
 
       const comparator = new BaseballNumbersComparator();
-      const result  = comparator.countStrikes(guessedNumbers, answerNumbers);
+      const result  = comparator.countStrikes(guess, answer);
 
       expect(result).toBe(1);
     });
   });
 
-  describe("countBalls()", () => {
-    test("두 개의 리스트에서 값은 포함하지만 위치는 다른 요소의 개수를 반환한다.", () => {
-      const guessedNumbers = [1, 5, 3];
-      const answerNumbers = [1, 3, 7];
+  describe("countIncludedNumbers()", () => {
+    test("두 리스트에서 중복된 요소의 개수를 반환한다.", () => {
+      const guess = new BaseballNumbers([
+        new BaseballNumber(1),
+        new BaseballNumber(5),
+        new BaseballNumber(3),
+      ]);
+      const answer = new BaseballNumbers([
+        new BaseballNumber(1),
+        new BaseballNumber(3),
+        new BaseballNumber(7),
+      ]);
 
       const comparator = new BaseballNumbersComparator();
-      const result  = comparator.countBalls(guessedNumbers, answerNumbers);
+      const result  = comparator.countIncludedNumbers(guess, answer);
 
-      expect(result).toBe(1);
+      expect(result).toBe(2);
     });
   });
 });
